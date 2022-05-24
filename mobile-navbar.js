ff69a90e0312cbf6b2b855230e6cab4f8a0d0bbc
EvanilsonPeso <filomenamatildek@gmail.com>
2022-05-24 02:41:50 -0300

    first commit

---
+++ mobile-navbar.js
@@ -0,0 +1,66 @@
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li",
+);
+mobileNavbar.init();
+
+Javascript
+let imagens= document.querySelectorAll('.small_img');
+let modal = document.querySelector('.modal');
+let modalImg = document.querySelector('#modal_img');
+let btClose = document.querySelector('#bt_close');
+let srcVal="";
+
+for(let i =0; i<imagens.length;i++){
+    imagens[i].addEventListener('click',function(){
+        
+        srcVal = imagens[i].getAttribute('src');
+        modalImg.setAttribute('src', srcVal);
+        modal.classList.toggle('modal_active');
+    });
+    
+}
+
+
+btClose.addEventListener('click', function(){
+    modal.classList.toggle('modal_active');
+});
